140ea2358ff92cfff64dee6e30d53d774a9df3f4
Batouuuuu <gillet.baptiste94@gmail.com>
2024-03-25 18:23:08 +0100

    nomenclature

---
+++ script/extractions_des_donnees.py
@@ -0,0 +1,11 @@
+## l'idée est de récuppérer des questions réponses sur ce forum 
+## https://www.forumconstruire.com/construire/topic-474537-meilleur-protection-anti-rouille.php#6365329 
+
+
+## comme il y'a beaucoup de réponses sur certaines questions il faudrait trouver un moyen de ne récuppérer que la meilleure réponse
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By 
+
+
+## chercher la voix de chatgpt pour voir si ça été écrit sur 
