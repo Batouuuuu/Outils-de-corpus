2a7f0366e296cdda840fdbb2e00a013c515c435e
Batouuuuu <gillet.baptiste94@gmail.com>
2024-04-17 22:16:13 +0200

    feat: récupération des meilleures réponses

--- script/extractions_des_donnees.py
+++ script/extractions_des_donnees.py
@@ -8,7 +8,7 @@
 
 
 from pathlib import Path
-from typing import List
+from typing import List, Set
 import requests
 from bs4 import BeautifulSoup
 from datastructures import Page
@@ -16,19 +16,19 @@ import csv
 
 
 
-def extraction_liens_forum(page_accueil : str | Path)-> List[str]:
+def extraction_liens_forum(page_accueil : str | Path)-> Set[str]:
     """Extrait les différents liens d'ouverture d'une discussion que l'on retrouve sur la 
     page principale du forum et les sauvegardes"""
     
-    liste_url = []
+    liste_url = set() ## je ne sais pas pourquoi j'avais des doublons j'ai donc tout stocké dans un set
     URL = requests.get(page_accueil) 
-    # print(URL.content) 
+    ##retirer les pubs
     liens_spam = ["https://www.forumconstruire.com/construire/devis-0-66-devis_renovation.php","https://achat.forumconstruire.com/vente-privee/"]
     soup = BeautifulSoup(URL.content, "html.parser")
     for lien in soup.find_all('a', class_='nounder'):
         url = lien.get('href')
         if url not in liens_spam:
-            liste_url.append(url)
+            liste_url.add(url)
 
     return liste_url
 
@@ -41,7 +41,7 @@ def sauvegardes_liens(chemin_fichier : str | Path ,liste_urls : List[str])-> Non
 
 
 def ouverture_liens(liste_url : List[str]) -> List[BeautifulSoup]:
-    """Ouvre les différents liens des discussions du forums"""
+    """Ouvre les différents liens des discussions du forums, prends tout leur contenu, balisage... et les stocke dans une liste"""
 
     liste_soup = []
     for lien in liste_url:
@@ -72,25 +72,63 @@ def sauvegardes_questions(chemin_fichier : str | Path, liste_question : List[str
             resultat.write(f"Question {numero}: {question}\n") 
 
 
-def recuperation_meilleure_reponse(soup_liste : List[BeautifulSoup]) -> List[str]:
-    """Récupération des meilleures réponses, 2 cas possibles : elle se trouve sur la même page, ou sur une
-    autre page"""
-
-    liste_reponse = []
-    for soup in soup_liste:
-        meilleure_reponse = soup.find("div", class_="post_resume_topic disnone")
-        if meilleure_reponse: ##cas où la meilleure réponse se trouve sur la même page
-            reponse = meilleure_reponse.get_text(separator=" ", strip=True) 
-            liste_reponse.appden(reponse)
-        else: #cas où la réponse se trouve sur une page différente du sujet
-            meilleure_reponse = soup.find("div", class_="rectangle_gris ultra_padding")
-            lien_rectangle = meilleure_reponse.find("a")
-            if lien_rectangle:
-                lien_rectangle_href = lien_rectangle.get("href")
-                recuperation_lien_meilleure_reponse = requests.get(lien_rectangle_href)
-                soup = BeautifulSoup(recuperation_lien_meilleure_reponse.content, "html.parser")
+def url_meilleurs_reponse(liste_url : Set[str]) -> List[str]:
+    """Récupération de l'URL de la meilleure réponse et ajout dans une liste"""
+    
+    url_meilleurs_reponse = []
+    print(len(liste_url))
+    for lien in liste_url:
+        response = requests.get(lien)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        div_tag = soup.find('div', class_='rectangle_gris ultra_padding')
+        
+        if div_tag:
+            a_tag = div_tag.find('a')
             
-    return liste_reponse
+            if a_tag:
+                str_base = 'https://www.forumconstruire.com/'
+                link = a_tag.get('href')
+                lien_complet = str_base + link  ## concaténation on ajoute https car le lien est un chemin relatif
+                url_meilleurs_reponse.append(lien_complet)
+                
+    return url_meilleurs_reponse
+
+
+
+def recuperation_meilleure_reponse(liste_URL_reponse : List[str]):
+    """Parcours l'URL où se trouve la meilleure réponse et récuppère la réponse avec le plus de likes"""
+    
+
+    comments_likes = []
+    liste_meilleure_reponse = []
+    print(len(liste_URL_reponse))
+    for lien in liste_URL_reponse:
+        print(lien)
+        response = requests.get(lien)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        # print(soup)
+        like_divs = soup.find_all('div', class_='post_ilike touch_links')
+
+        ## parcours de tous les commentaires et de leur nombre de like et ajout dans un tuple
+        nombre_like = 0
+        meilleure_reponse = ""
+        for div in like_divs:
+            like_count = int(div.find('span', class_='like_text').text.strip())
+            comment_text = div.find_previous('div', class_='postsimple_message_cell').get_text(separator=" ", strip=True)
+            tuple = (like_count, comment_text)
+            comments_likes.append(tuple)
+            # print(comments_likes)
+    
+            ##récupération du commentaire avec le plus de like 
+            if like_count > nombre_like:
+                nombre_like = like_count
+                meilleure_reponse = comment_text
+        liste_meilleure_reponse.append(meilleure_reponse)
+
+    return liste_meilleure_reponse
+
+
 
 def extraction_question_reponse_chatgpt():
     NotImplemented
@@ -113,12 +151,17 @@ def ecriture_resultat_tsv(dataset_fichier : str | Path, questions : List[str]) -
 def main():
 
     resultats_extraction = extraction_liens_forum("https://www.forumconstruire.com/construire/forum-51.php")
-    sauvegardes_liens("../data/raw/liens.txt", resultats_extraction)
+
     soup_liste = ouverture_liens(resultats_extraction)
-    questions = recuperation_question(soup_liste)
-    sauvegardes_questions("../data/raw/questions.txt", questions)
-    ecriture_resultat_tsv('../data/dataset/dataset.csv',questions)
-    # recuperation_meilleure_reponse(soup_liste)
+
+    reponses_URL = url_meilleurs_reponse(resultats_extraction)
+    test = recuperation_meilleure_reponse(reponses_URL)
+    print(test)
+
+
+    # sauvegardes_questions("../data/raw/questions.txt", questions)
+    # ecriture_resultat_tsv('../data/dataset/dataset.csv',questions)
+    # sauvegardes_liens("../data/raw/liens.txt", resultats_extraction)
 
 if __name__ == "__main__":
     main()
